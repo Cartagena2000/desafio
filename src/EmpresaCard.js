82b4f1f22d1a6ed53ec8c89a0a6a3a62e01c580d
Cartagena2000 <josecarta2000@gmail.com>
2024-07-27 10:50:06 -0600

    nuevo Json

--- src/EmpresaCard.js
+++ src/EmpresaCard.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./EmpresaCard.css";
 
-const EmpresaCard = ({ titulo, descripcion, caracteristicas, imagen }) => {
+const EmpresaCard = ({
+  titulo,
+  descripcion,
+  caracteristicas,
+  imagen_de_referencia,
+}) => {
+  // Desestructuramos las características del objeto
+  const { fecha, impacto } = caracteristicas;
+
   return (
     <div className="empresa-card">
       <div className="empresa-card__contenido">
@@ -9,9 +17,20 @@ const EmpresaCard = ({ titulo, descripcion, caracteristicas, imagen }) => {
         <h4 className="empresa-card__subtitulo">DESCRIPCIÓN</h4>
         <p className="empresa-card__descripcion">{descripcion}</p>
         <h4 className="empresa-card__subtitulo">CARACTERÍSTICAS</h4>
-        <p className="empresa-card__caracteristicas">{caracteristicas}</p>
+        <ul className="empresa-card__caracteristicas">
+          <li>
+            <strong>Fecha:</strong> {fecha}
+          </li>
+          <li>
+            <strong>Impacto:</strong> {impacto}
+          </li>
+        </ul>
       </div>
-      <img src={imagen} alt={titulo} className="empresa-card__imagen" />
+      <img
+        src={imagen_de_referencia}
+        alt={titulo}
+        className="empresa-card__imagen"
+      />
     </div>
   );
 };
